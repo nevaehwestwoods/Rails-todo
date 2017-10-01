1035a43f60210ca2d09cbdf790073789258cf5c3
Nevaeh <nevaehwestwoods@gmail.com>
2017-10-01 18:43:50 +0000

    update todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,4 +15,14 @@ class TodosController < ApplicationController
             )
         redirect_to "/todos/#{todo.id}"    
     end
+        def update
+            todo=Todo.find(params[:id])
+            todo.update(
+                name: params[:name],
+                description: params[:description],
+                duration: params[:duration],
+                complete: params[:complete]
+                )
+                redirect_to "/todos/#{todo.id}"
+        end
 end
